66e642b686446069c7dc98af52826ff19ff6464d
Saanvi Chabaque <145357655+Schabaque@users.noreply.github.com>
2024-10-20 20:20:04 +0530

    Update PrevBox.jsx
    PREV BOX SIZE INC

--- src/Components/PrevSpeakers/PrevBox.jsx
+++ src/Components/PrevSpeakers/PrevBox.jsx
@@ -1,17 +1,17 @@
 const PrevBox = ({ imgSrc, altText, name, description }) => {
   return (
-    <div className="hover:-translate-y-3 transition-transform duration-300  ">
-      <div className="m-4 mb-3 group relative border-[1px] border-bordorColor rounded-[15px] w-[80%] md:w-[200px] h-[80%] md:h-[200px] p-2 bg-[#141414] overflow-hidden">
+    <div className="hover:-translate-y-3 transition-transform duration-300">
+      <div className="m-4 group relative border-4 border-[#141414] rounded-2xl w-[80%] md:w-[250px] h-[80%] md:h-[250px] p-1 bg-[#141414] overflow-hidden">
         <img
-          className="rounded-[14px] transition-opacity duration-300  group-hover:opacity-0 w-full h-full object-cover"
+          className="rounded-2xl transition-opacity duration-300 group-hover:opacity-0 w-full h-full object-cover"
           src={imgSrc}
           alt={altText}
         />
         <div className="absolute inset-0 hidden group-hover:flex items-center justify-center bg-[#141414] p-1 rounded-2xl transition-opacity duration-300">
-          <p className="text-white">{description}</p>
+          <p className="text-white text-center"><h2 className="m-4">{name}</h2>{description}</p>
         </div>
       </div>
-      <h2 className="ml-4 text-md text-[#7d7d7d] text-left  ">
+      <h2 className="text-xl text-[#7d7d7d] text-left  pl-1 md:pl-10">
         {name}
       </h2>
     </div>
