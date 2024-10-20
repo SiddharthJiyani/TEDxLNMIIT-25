fc155aa268f3b578c597d808e7675cc243ae2f3d
SiddharthJiyani <sidjiyani2003@gmail.com>
2024-10-20 08:58:59 +0530

    Navbar bg changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,8 @@ import { faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
 const Navbar = () => {
   return (
     <nav className="bg-[rgba(20,20,20,0.5)] backdrop-blur-md border border-white/20 p-2 sm:p-3 md:p-4 lg:p-2 fixed left-0 right-0 mx-auto mt-6 max-w-[90%] min-h-[48px] flex items-center justify-between gap-4 rounded-[60px] shadow-lg z-50">
+      <div className="container mx-auto flex justify-between items-center">
+        
         <div className="text-white text-xl font-bold flex items-center">
           <Link to="/" className="text-xl text-white">
             <img src="/logo.png" alt="TEDx Logo" className="h-9 mr-3 ml-3" />
@@ -39,8 +41,7 @@ const Navbar = () => {
             href="https://www.linkedin.com/company/tedxlnmiit/"
             target="_blank"
             rel="noopener noreferrer"
-            className="border border-gray-600 rounded-full p-2 flex items-center justify-center hover:opacity-65 transition-all duration-500"
-          >
+            className="border border-gray-600 rounded-full p-2 flex items-center justify-center hover:opacity-65 transition-all duration-500">
             <FontAwesomeIcon
               icon={faLinkedin}
               className="text-gray-300 h-7 w-7"
@@ -50,8 +51,7 @@ const Navbar = () => {
             href="https://www.instagram.com/tedxlnmiit/"
             target="_blank"
             rel="noopener noreferrer"
-            className="border border-gray-600 rounded-full p-2 flex items-center justify-center hover:opacity-65 transition-all duration-500"
-          >
+            className="border border-gray-600 rounded-full p-2 flex items-center justify-center hover:opacity-65 transition-all duration-500">
             <FontAwesomeIcon
               icon={faInstagram}
               className="text-gray-300 h-7 w-7"
@@ -61,8 +61,7 @@ const Navbar = () => {
           <div className=" flex items-center justify-center border border-gray-600 rounded-[60px]  p-1 hover:opacity-65 transition-all duration-500">
             <Link
               to="/contact"
-              className=" text-white font-semibold px-4 py-1 rounded-full"
-            >
+              className=" text-white font-semibold px-4 py-1 rounded-full">
               Contact Now
             </Link>
             <img
