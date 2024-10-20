030f65ffb01f5b0bb47cdbcab803fbfc16cb1b5b
Saanvi Chabaque <145357655+Schabaque@users.noreply.github.com>
2024-10-20 20:20:41 +0530

    Update PrevSpeakers.jsx

--- src/Components/PrevSpeakers/PrevSpeakers.jsx
+++ src/Components/PrevSpeakers/PrevSpeakers.jsx
@@ -1,7 +1,12 @@
 import speaker1 from "../../assets/speaker1.jpg";
 import speaker2 from "../../assets/speaker2.png";
 import speaker3 from "../../assets/speaker3.png";
+import speaker4 from "../../assets/speaker4.webp";
+import speaker5 from "../../assets/speaker5.webp";
+import speaker6 from "../../assets/speaker6.webp";
+import speaker7 from "../../assets/speaker7.webp";
 import speaker8 from "../../assets/speaker8.jpg";
+import speaker9 from "../../assets/speaker9.webp";
 import PrevBox from "./PrevBox.jsx";
 
 const PrevSpeakers = () => {
@@ -15,15 +20,16 @@ const PrevSpeakers = () => {
             imgSrc={speaker1}
             altText="Speaker"
             name="Dr. Tanu Jain"
-            description="Dr. Tanu Jain, an accomplished IAS Officer, transitioned from dentistry to civil service, exemplifying excellence and community service."
+            description="Dr. Tanu is currently posted as Assistant Director in the Ministry of Defence. Dr. Tanu has authored two books for UPSC preparation. She is Passionate to help, guiding, and motivating young minds esp."
           />
 
           {/* Second Speaker */}
+
           <PrevBox
             imgSrc={speaker2}
             altText="Speaker"
             name="Krishnan Sunderarajan"
-            description="Krishnan Sunderarajan is the visionary mind behind India's First Metaverse App, LOKA, as showcased on Shark Tank."
+            description="Krishnan Sunderarajan is the visionary mind behind India's First Metaverse App, LOKA, as showcased on Shark Tank. A seasoned entrepreneur, reshaping the landscape of technology and gaming. "
           />
 
           {/* Third Speaker */}
@@ -31,40 +37,40 @@ const PrevSpeakers = () => {
             imgSrc={speaker3}
             altText="Speaker"
             name="Palakh Khanna"
-            description="Palakh Khanna, a trailblazing entrepreneur and esteemed speaker, is a beacon of innovation and inspiration."
+            description="Palakh Khanna, a trailblazing entrepreneur and esteemed speaker, is a beacon of innovation and inspiration. Recognized as one of Asia's 100 Women Power Leaders in 2023, her journey with Break."
           />
 
           {/* Fourth Speaker */}
           <PrevBox
-            imgSrc={speaker1}
+            imgSrc={speaker4}
             altText="Speaker"
-            name="Dr. Tanu Jain"
-            description="This text appears when the image is hidden."
+            name="Chinmay Gaur"
+            description="Chinmay Gaur,skillfully wields the flute to enrapture audiences with his enchanting melodies and charismatic presence. As an Indian Classical flutist , esteemed music composer, creating a captivating musical narrative."
           />
         </div>
         <div className="flex flex-wrap justify-center">
           {/* Fifth Speaker */}
           <PrevBox
-            imgSrc={speaker1}
+            imgSrc={speaker5}
             altText="Speaker"
-            name="Dr. Tanu Jain"
-            description="This text appears when the image is hidden."
+            name="Deepak Pareek"
+            description="Deepak Pareekh is a renowned seven-time TEDx speaker and co-founder of Iceberg Creations. Deepak is a renowned content creator who was featured in IMPACT's 30 Under 30"
           />
 
           {/* Sixth Speaker */}
           <PrevBox
-            imgSrc={speaker1}
+            imgSrc={speaker6}
             altText="Speaker"
-            name="Dr. Tanu Jain"
-            description="This text appears when the image is hidden."
+            name="Tejas Patil"
+            description="Tejas Patil is a prominent figure in the tech world, celebrated for his insightful content creation and social media strategy. With hands-on expertise in PCs and smartphones, his authentic reviews are highly respected."
           />
 
           {/* Seventh Speaker */}
           <PrevBox
-            imgSrc={speaker1}
+            imgSrc={speaker7}
             altText="Speaker"
-            name="Dr. Tanu Jain"
-            description="This text appears when the image is hidden."
+            name="Gunjan Saini"
+            description="Gunjan Saini is a multifaceted Indian poet and author. Beyond her literary prowess, Gunjan's talents span across various domains as an actor, dancer, journalist, licensed practical nurse, and Filtercopy's Casting Associate."
           />
 
           {/* Eighth Speaker */}
@@ -72,8 +78,20 @@ const PrevSpeakers = () => {
             imgSrc={speaker8}
             altText="Speaker"
             name="Riya Upreti"
-            description="This text appears when the image is hidden."
+            description="Riya Upreti, influencer and CEO of Thefobet, is reshaping education in Delhi. FOBET, her brainchild, revolutionizes the learning landscape, particularly in psychology-related knowledge. "
           />
+
+          {/* Ninth Speaker */}
+          <PrevBox
+            imgSrc={speaker9}
+            altText="Speaker"
+            name="Sakshi Mandhyan"
+            description="Sakshi Mandhyan, an accomplished professional, is
+an established Psychologist, Happiness Coach, and
+Wellness Expert with 15+ years of Fortune 500
+experience."
+          />
+
         </div>
       </div>
     </div>
