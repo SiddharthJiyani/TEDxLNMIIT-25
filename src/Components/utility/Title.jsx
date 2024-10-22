b3150489821f08f00bd6b594803ad33f86330435
SiddharthJiyani <sidjiyani2003@gmail.com>
2024-10-22 10:07:17 +0530

    commit

--- src/Components/utility/Title.jsx
+++ src/Components/utility/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = ({ text, className = '' }) => {
   return (
-    <h2 className={`text-[56px] mt-10 leading-[64px] font-medium text-transparent bg-clip-text bg-Title-gradient ${className}`}>
+    <h2 className={`text-[60px] mt-10 leading-[64px] font-medium text-transparent bg-clip-text bg-heading-gradient ${className}`}>
       {text}
     </h2>
   );
