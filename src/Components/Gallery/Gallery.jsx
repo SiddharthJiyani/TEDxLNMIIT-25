bf70c6b915ad57304ce16a8a4b9a3b6064622e5e
Saanvi Chabaque <145357655+Schabaque@users.noreply.github.com>
2024-10-23 02:17:05 +0530

    Update Gallery.jsx

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,5 +1,29 @@
+import img from '../../assets/pic1.jpg';
+import PrevBox from "../PrevSpeakers/PrevBox";
+
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <div className='relative h-screen flex flex-col items-center justify-center bg-customDark backdrop-blur-md '>
+      <div className='absolute  top-20  bg-customDark backdrop-blur-md '>
+      <div className='flex flex-wrap justify-center gap-4'>
+        <PrevBox imgSrc={img} />
+        <PrevBox imgSrc={img} />
+        <PrevBox imgSrc={img} />
+      </div>
+      <div className='flex flex-wrap justify-center gap-4 mt-4'>
+        <PrevBox imgSrc={img} />
+        <PrevBox imgSrc={img} />
+        <PrevBox imgSrc={img} />
+      </div>
+      <div className='flex flex-wrap justify-center gap-4 mt-4'>
+        <PrevBox imgSrc={img} />
+        <PrevBox imgSrc={img} />
+        <PrevBox imgSrc={img} />
+      </div>
+    </div>
+    </div>
+  );
 };
 
 export default Gallery;
+
