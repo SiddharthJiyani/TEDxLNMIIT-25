67f14c4b8a4fb531dcb94aeece99de9d8f34c05c
Saanvi Chabaque <145357655+Schabaque@users.noreply.github.com>
2024-10-23 02:18:45 +0530

    Update PrevBox.jsx

--- src/Components/PrevSpeakers/PrevBox.jsx
+++ src/Components/PrevSpeakers/PrevBox.jsx
@@ -1,22 +1,38 @@
-const PrevBox = ({ imgSrc, altText, name, description }) => {
+import youtube from '../../assets/youtube.png';
+
+const PrevBox = ({ imgSrc, altText, name, description, url }) => {
   return (
     <div className="hover:-translate-y-3 transition-transform duration-300">
-      <div className="m-4 group relative border-[1px] border-bordorColor rounded-2xl w-[80%] sm:w-[150px] md:w-[250px] h-[80%] sm:h-[150px] md:h-[250px] p-1 bg-[#141414] overflow-hidden">
+      <div className="m-4 group relative border-4 border-[#141414] rounded-2xl w-[80%] md:w-[250px] h-[80%] md:h-[290px] p-1 bg-[#141414] overflow-hidden">
+        {/* Image always shown */}
         <img
-          className="rounded-2xl transition-opacity duration-300 group-hover:opacity-0 w-full h-full object-scale-down"
+          className={`rounded-2xl transition-opacity duration-300 w-full h-full object-cover ${description || url ? 'group-hover:opacity-0' : ''}`}
           src={imgSrc}
           alt={altText}
         />
-        <div className="absolute inset-0 hidden group-hover:flex items-center justify-center bg-[#141414] p-1 rounded-2xl  transition-opacity duration-300">
-          <p className=" text-center font-thin text-[#a5a1a1]">
-            <h1 className="m-4 text-xl font-extrabold text-[#e4dbdb]">
-              {name}
-            </h1>
-            {description}
-          </p>
-        </div>
+
+        {/* Conditional hover content */}
+        {(description || url) && (
+          <div className="absolute inset-0 hidden group-hover:flex items-center justify-center bg-[#141414] p-1 rounded-2xl transition-opacity duration-300">
+            <div className="text-white text-center">
+              <h2 className="m-4">{name}</h2>
+              {description && <p>{description}</p>}
+              {url && (
+                <a
+                  className="flex justify-center items-center"
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img className="h-6 w-6 m-2" src={youtube} alt="YouTube logo" />
+                  Watch Talk here
+                </a>
+              )}
+            </div>
+          </div>
+        )}
       </div>
-      <h2 className="text-xl text-[#7d7d7d] text-left  pl-1 md:pl-10">
+      <h2 className="text-xl text-[#7d7d7d] text-left pl-1 md:pl-10">
         {name}
       </h2>
     </div>
