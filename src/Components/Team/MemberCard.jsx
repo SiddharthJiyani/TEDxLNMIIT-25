d69c64bb4ad90cde40de9d78399062ac17c7371b
992manav <dmanavpianist@gmail.com>
2024-11-12 10:42:21 +0530

    team link

--- src/Components/Team/MemberCard.jsx
+++ src/Components/Team/MemberCard.jsx
@@ -1,7 +1,9 @@
 import manav from "../../assets/team/manav.jpg";
-const MemberCard = ({ name, position }) => {
+import { FaLinkedin } from "react-icons/fa"; // Importing the LinkedIn icon from react-icons.
+
+const MemberCard = ({ name, position, link }) => {
   return (
-    <div className="flex flex-col items-center bg-black p-4 rounded-lg">
+    <div className="relative group flex flex-col items-center bg-black p-4 rounded-lg hover:opacity-90">
       {/* Image section */}
       <div className="w-40 h-40 rounded-full overflow-hidden border-4 border-gray-800">
         <img src={manav} alt={name} className="w-full h-full object-cover" />
@@ -12,6 +14,21 @@ const MemberCard = ({ name, position }) => {
 
       {/* Title section */}
       <p className="text-lg italic text-gray-400">{position}</p>
+
+      {/* Hovered Component */}
+      <div className="absolute mt-4  ml-4 w-40 h-40 inset-0 rounded-full flex flex-col items-center justify-center  bg-gradient-to-b from-[#DA0400] to-[#890300]  opacity-0 transition-all duration-300 group-hover:opacity-100 ">
+        <h3 className="select-none  text-center text-sm/[110%] capitalize leading-none text-white md:text-lg/[110%] md:leading-tight word-break mb-2 ">
+          {name}
+        </h3>
+
+        {link && (
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <div className="w-12 h-12 rounded-full flex items-center justify-center bg-black text-red-500 translate-y-10 duration-300 ease-out group-hover:translate-y-0 grayscale hover:grayscale-0">
+              <FaLinkedin size={24} />
+            </div>
+          </a>
+        )}
+      </div>
     </div>
   );
 };
--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -10,10 +10,22 @@ const Team = () => {
       {/* Organizer */}
       <h2 className="text-2xl font-semibold mt-10 mb-6">Organizer</h2>
       <div className="flex flex-wrap justify-center gap-6">
-        <MemberCard name="Abhey Raheja" />
-        <MemberCard name="Abhinav Singh" />
-        <MemberCard name="Krishna Rohira" />
-        <MemberCard name="Suhani Gupta" />
+        <MemberCard
+          name="Abhey Raheja"
+          link="https://www.linkedin.com/in/abhey-raheja"
+        />
+        <MemberCard
+          name="Abhinav Singh"
+          link="https://www.linkedin.com/in/abhinav-singh"
+        />
+        <MemberCard
+          name="Krishna Rohira"
+          link="https://www.linkedin.com/in/krishna-rohira"
+        />
+        <MemberCard
+          name="Suhani Gupta"
+          link="https://www.linkedin.com/in/suhani-gupta"
+        />
       </div>
 
       <div className="flex flex-wrap justify-between">
@@ -23,7 +35,10 @@ const Team = () => {
             Web Development Head
           </h2>
           <div className="flex justify-center gap-6">
-            <MemberCard name="Siddharth Jiyani" />
+            <MemberCard
+              name="Siddharth Jiyani"
+              link="https://www.linkedin.com/in/siddharth-jiyani"
+            />
           </div>
         </div>
 
@@ -33,7 +48,10 @@ const Team = () => {
             Content Writing Head
           </h2>
           <div className="flex justify-center gap-6">
-            <MemberCard name="Sahil Gaur" />
+            <MemberCard
+              name="Sahil Gaur"
+              link="https://www.linkedin.com/in/sahil-gaur"
+            />
           </div>
         </div>
 
@@ -43,7 +61,10 @@ const Team = () => {
             Sponsorship Head
           </h2>
           <div className="flex justify-center gap-6">
-            <MemberCard name="Shreyansh Badoni" />
+            <MemberCard
+              name="Shreyansh Badoni"
+              link="https://www.linkedin.com/in/shreyansh-badoni"
+            />
           </div>
         </div>
       </div>
@@ -53,8 +74,14 @@ const Team = () => {
         <div className="w-full md:w-1/2 px-4">
           <h2 className="text-2xl font-semibold mt-10 mb-6">Creative Head</h2>
           <div className="flex flex-wrap justify-center gap-6">
-            <MemberCard name="Krishna Manchanda" />
-            <MemberCard name="Divay Yadav" />
+            <MemberCard
+              name="Krishna Manchanda"
+              link="https://www.linkedin.com/in/krishna-manchanda"
+            />
+            <MemberCard
+              name="Divay Yadav"
+              link="https://www.linkedin.com/in/divay-yadav"
+            />
           </div>
         </div>
 
@@ -64,7 +91,10 @@ const Team = () => {
             Event Management Head
           </h2>
           <div className="flex flex-wrap justify-center gap-6">
-            <MemberCard name="Nemil Kamdar" />
+            <MemberCard
+              name="Nemil Kamdar"
+              link="https://www.linkedin.com/in/nemil-kamdar"
+            />
           </div>
         </div>
       </div>
@@ -74,18 +104,33 @@ const Team = () => {
         <div className="w-full md:w-1/2 px-4">
           <h2 className="text-2xl font-semibold mt-10 mb-6">Web Development</h2>
           <div className="flex flex-wrap justify-center gap-6">
-            <MemberCard name="Saanvi Chabaque" />
-            <MemberCard name="Manav Desai" />
+            <MemberCard
+              name="Saanvi Chabaque"
+              link="https://www.linkedin.com/in/saanvi-chabaque"
+            />
+            <MemberCard
+              name="Manav Desai"
+              link="https://www.linkedin.com/in/manav-desai"
+            />
           </div>
         </div>
 
         {/* Content Writing */}
         <div className="w-full md:w-1/2 px-4">
           <h2 className="text-2xl font-semibold mt-10 mb-6">Content Writing</h2>
-          <div className="flex flex-wrap justify-center ">
-            <MemberCard name="Aryan Jadhav" />
-            <MemberCard name="Alisha Malhotra" />
-            <MemberCard name="Abdul Hadi" />
+          <div className="flex flex-wrap justify-center gap-6">
+            <MemberCard
+              name="Aryan Jadhav"
+              link="https://www.linkedin.com/in/aryan-jadhav"
+            />
+            <MemberCard
+              name="Alisha Malhotra"
+              link="https://www.linkedin.com/in/alisha-malhotra"
+            />
+            <MemberCard
+              name="Abdul Hadi"
+              link="https://www.linkedin.com/in/abdul-hadi"
+            />
           </div>
         </div>
       </div>
@@ -96,10 +141,19 @@ const Team = () => {
           <h2 className="text-2xl font-semibold mb-4 text-center">
             Speaker Outreach
           </h2>
-          <div className="flex flex-wrap justify-center ">
-            <MemberCard name="Anshika Agrawal" />
-            <MemberCard name="Sakash Srivastava" />
-            <MemberCard name="Priyal Maheshwari" />
+          <div className="flex flex-wrap justify-center gap-6">
+            <MemberCard
+              name="Anshika Agrawal"
+              link="https://www.linkedin.com/in/anshika-agrawal"
+            />
+            <MemberCard
+              name="Sakash Srivastava"
+              link="https://www.linkedin.com/in/sakash-srivastava"
+            />
+            <MemberCard
+              name="Priyal Maheshwari"
+              link="https://www.linkedin.com/in/priyal-maheshwari"
+            />
           </div>
         </div>
 
@@ -108,10 +162,19 @@ const Team = () => {
           <h2 className="text-2xl font-semibold mb-4 text-center">
             Graphic Designing
           </h2>
-          <div className="flex flex-wrap justify-center ">
-            <MemberCard name="Sanskriti Arora" />
-            <MemberCard name="Akshat Gupta" />
-            <MemberCard name="Hussain Haidary" />
+          <div className="flex flex-wrap justify-center gap-6">
+            <MemberCard
+              name="Sanskriti Arora"
+              link="https://www.linkedin.com/in/sanskriti-arora"
+            />
+            <MemberCard
+              name="Akshat Gupta"
+              link="https://www.linkedin.com/in/akshat-gupta"
+            />
+            <MemberCard
+              name="Hussain Haidary"
+              link="https://www.linkedin.com/in/hussain-haidary"
+            />
           </div>
         </div>
       </div>
@@ -119,19 +182,40 @@ const Team = () => {
       {/* Event Management */}
       <h2 className="text-2xl font-semibold mt-10 mb-6">Event Management</h2>
       <div className="flex flex-wrap justify-center gap-6">
-        <MemberCard name="Nancy" />
-        <MemberCard name="Rahul Sharma" />
-        <MemberCard name="Naman Agarwal" />
-        <MemberCard name="Anmol Adwani" />
+        <MemberCard name="Nancy" link="https://www.linkedin.com/in/nancy" />
+        <MemberCard
+          name="Rahul Sharma"
+          link="https://www.linkedin.com/in/rahul-sharma"
+        />
+        <MemberCard
+          name="Naman Agarwal"
+          link="https://www.linkedin.com/in/naman-agarwal"
+        />
+        <MemberCard
+          name="Anmol Adwani"
+          link="https://www.linkedin.com/in/anmol-adwani"
+        />
       </div>
 
       {/* Sponsorship */}
       <h2 className="text-2xl font-semibold mt-10 mb-6">Sponsorship</h2>
       <div className="flex flex-wrap justify-center gap-6">
-        <MemberCard name="Vihaan Malik" />
-        <MemberCard name="Parv Khandelwal" />
-        <MemberCard name="Pratham Gupta" />
-        <MemberCard name="Nirmal Patel" />
+        <MemberCard
+          name="Vihaan Malik"
+          link="https://www.linkedin.com/in/vihaan-malik"
+        />
+        <MemberCard
+          name="Parv Khandelwal"
+          link="https://www.linkedin.com/in/parv-khandelwal"
+        />
+        <MemberCard
+          name="Pratham Gupta"
+          link="https://www.linkedin.com/in/pratham-gupta"
+        />
+        <MemberCard
+          name="Nirmal Patel"
+          link="https://www.linkedin.com/in/nirmal-patel"
+        />
       </div>
 
       <div className="flex flex-wrap justify-between p-6">
@@ -141,7 +225,10 @@ const Team = () => {
             Social Media
           </h2>
           <div className="flex justify-center gap-6">
-            <MemberCard name="Kanishq Malhotra" />
+            <MemberCard
+              name="Kanishq Malhotra"
+              link="https://www.linkedin.com/in/kanishq-malhotra"
+            />
           </div>
         </div>
 
@@ -150,10 +237,15 @@ const Team = () => {
           <h2 className="text-2xl font-semibold mb-4 text-center">
             Video Editing
           </h2>
-          <div className="flex flex-wrap justify-center space-x-4 gap-6">
-            <MemberCard name="Kunal Sharma" />
-            <MemberCard name="Chirag Mehta" />
-            <MemberCard name="Yash Jain" />
+          <div className="flex flex-wrap justify-center gap-6">
+            <MemberCard
+              name="Kunal Sharma"
+              link="https://www.linkedin.com/in/kunal-sharma"
+            />
+            <MemberCard
+              name="Chirag Mehta"
+              link="https://www.linkedin.com/in/chirag-mehta"
+            />
           </div>
         </div>
       </div>
