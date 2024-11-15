bd5f7c2ac08454630878215c00306dc61e990193
Saanvi Chabaque <145357655+Schabaque@users.noreply.github.com>
2024-11-15 16:06:31 +0530

    Update SpeakerList.jsx

--- src/Components/Speakers/SpeakerList.jsx
+++ src/Components/Speakers/SpeakerList.jsx
@@ -6,7 +6,7 @@ import speaker5 from "../../assets/PastSpeakers/speaker5.png";
 import speaker6 from "../../assets/PastSpeakers/speaker6.png";
 import speaker7 from "../../assets/PastSpeakers/speaker7.png";
 import speaker9 from "../../assets/PastSpeakers/speaker9.png";
-import Speaker from "./Speaker.jsx";
+import Speakers from "./Speakers.jsx";
 
 const SpeakerList = () => {
   return (
@@ -16,29 +16,29 @@ const SpeakerList = () => {
         <div className="flex flex-wrap justify-center">
           {/* First Speaker */}
         
-          <Speaker imgSrc={speaker1} altText="Speaker" name="Dr. Tanu Jain" />
+          <Speakers imgSrc={speaker1} altText="Speaker" name="Dr. Tanu Jain" />
 
           {/* Second Speaker */}
-          <Speaker imgSrc={speaker2} altText="Speaker" name="Krishnan Sunderarajan" />
+          <Speakers imgSrc={speaker2} altText="Speaker" name="Krishnan Sunderarajan" />
 
           {/* Third Speaker */}
-          <Speaker imgSrc={speaker3} altText="Speaker" name="Palakh Khanna" />
+          <Speakers imgSrc={speaker3} altText="Speaker" name="Palakh Khanna" />
 
           {/* Fourth Speaker */}
-          <Speaker imgSrc={speaker4} altText="Speaker" name="Chinmay Gaur" />
+          <Speakers imgSrc={speaker4} altText="Speaker" name="Chinmay Gaur" />
         </div>
         <div className="flex flex-wrap justify-center">
           {/* Fifth Speaker */}
-          <Speaker imgSrc={speaker5} altText="Speaker" name="Deepak Pareek" />
+          <Speakers imgSrc={speaker5} altText="Speaker" name="Deepak Pareek" />
 
           {/* Sixth Speaker */}
-          <Speaker imgSrc={speaker6} altText="Speaker" name="Tejas Patil" />
+          <Speakers imgSrc={speaker6} altText="Speaker" name="Tejas Patil" />
 
           {/* Seventh Speaker */}
-          <Speaker imgSrc={speaker7} altText="Speaker" name="Gunjan Saini" />
+          <Speakers imgSrc={speaker7} altText="Speaker" name="Gunjan Saini" />
 
           {/* Eighth Speaker */}
-          <Speaker imgSrc={speaker9} altText="Speaker" name="Sakshi Mandhyan" />
+          <Speakers imgSrc={speaker9} altText="Speaker" name="Sakshi Mandhyan" />
         </div>
       </div>
     </div>
